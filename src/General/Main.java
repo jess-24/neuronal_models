d3bbb027dc20f3dd26a1b97ab9ed4ce7b59ddc97
jess-24 <jess-24@outlook.com>
2019-04-07 19:02:56 -0500

    modelo hopfield en proceso

--- src/General/Main.java
+++ src/General/Main.java
@@ -2,9 +2,14 @@ package General;
 
 import java.util.Scanner;
 import perceptron.*;
+import hopfield.*;
+
+import javax.sound.midi.Soundbank;
+
 public class Main {
     entrada entrada;
     entrenamiento entrenamiento;
+    entrenaHopfield hopfield;
     Main(){
         System.out.println("****Elije un modelo***");
         System.out.println("1.Perceptron");
@@ -29,6 +34,26 @@ public class Main {
     public void  modelo_Hopfield(Scanner s){
         entrada=new entrada(s);
         entrada.definirPatronesE();
+        hopfield=new entrenaHopfield();
+        hopfield.fase_Recuperacion(entrada.getArrayPatrones_k().size(),entrada.getNeuronas_x());
+        if (hopfield.getBuena_Recu()){
+            System.out.println("Buena recuperación!");
+            if (hopfield.getExcelente_Recu()){
+                System.out.println("Excelente recuperación");
+            }else {System.out.println("No tiene Excelente Recuperación"); }
+            if (hopfield.ortogonalidad(entrada.getArrayPatrones_k(),entrada.neuronas_x)){
+                System.out.println("Cumple con ortogonalidad minima del 50%");
+                //proceso
+                hopfield.matriz_PesosW(entrada.getNeuronas_x(),entrada.getArrayPatrones_k());
+            }else {
+                System.out.println("No cumple con ortogonalidad minima del 50%, cambiar patrones");
+                modelo_Hopfield(s);
+            }
+        }else {
+            System.out.println("No tiene buena recuperación");
+            System.out.println("Cambiar número de neuronas");
+            modelo_Hopfield(s);
+        }
     }
     public static void main(String[]args){
         Main obj=new Main();
--- src/hopfield/entrenaHopfield.java
+++ src/hopfield/entrenaHopfield.java
@@ -1,17 +1,60 @@
 package hopfield;
+import General.*;
+import java.util.ArrayList;
 
 public class entrenaHopfield {
     boolean buena_recuepracion;
     boolean exel_recu;
+    double ortogonalidad;
+    double[][] matriz_pesos;
     public void fase_Recuperacion(int m_patrones,int neuronas_N){
         System.out.println("****************");
         System.out.println("FASE DE RECUPERACIÓN");
         if (m_patrones <= (0.138*neuronas_N)){
             buena_recuepracion=true;
-        }else { buena_recuepracion=false}
+        }else { buena_recuepracion=false;}
         if (m_patrones<= (neuronas_N/(4*Math.log(neuronas_N)))){
             exel_recu=true;
         }else {exel_recu=false;}
-        
+    }
+    public boolean getBuena_Recu(){
+        return buena_recuepracion;
+    }
+    public boolean getExcelente_Recu(){
+        return exel_recu;
+    }
+    public boolean ortogonalidad(ArrayList<patron> patrones, int neuronas){
+        ortogonalidad=0;
+        for (int n=0; n<neuronas; n++) {//maneja neuronas
+            for (int p=0; p< patrones.size(); p++) {//maneja patrones
+                ortogonalidad+=patrones.get(p).getNeuronas().get(n);
+            }
+        }//ortogonalidad menos al 50%
+        if (ortogonalidad<=0)
+            return true;
+        else return false;
+    }
+    public void matriz_PesosW(int neuronas,ArrayList<patron> patrones){
+        matriz_pesos=new double[neuronas][neuronas];
+        //cada patron obtejer pkt* pk -I
+        for (int p=0; p<patrones.size();p++){//por cada patron
+        for (int i=0; i<neuronas;i++){//suma patrones
+            for (int j=0; j<neuronas;j++){
+                if (i!=j) {
+                    for (int aux=0; aux<neuronas; aux++) {
+                        matriz_pesos[i][j] += patrones.get(p).getNeuronas().get(j) * patrones.get(p).getNeuronas().get(aux);
+
+                    }
+                    System.out.print(matriz_pesos[i][j]+"  ");
+                }else{ matriz_pesos[i][j]=0;}
+            }
+            System.out.print("\n");
+        }}
+
+        for (int i=0; i<neuronas;i++){//suma patrones
+            for (int j=0; j<neuronas;j++){
+            System.out.print(matriz_pesos[i][j]+"  ");}
+            System.out.print("\n");
+        }
     }
 }
